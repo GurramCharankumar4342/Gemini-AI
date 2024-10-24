b66edc144ce87ed624b14e109d28f7959da8abae
Charan <gurramcharan0@gmail.com>
2024-10-24 10:40:29 +0530

    first commit

---
+++ src/config/gemini.js
@@ -0,0 +1,47 @@
+
+import {
+  GoogleGenerativeAI,
+  HarmCategory,
+  HarmBlockThreshold,
+} from "@google/generative-ai"
+
+const apiKey = import.meta.env.VITE_GOOGLE_API_KEY;
+const genAI = new GoogleGenerativeAI(apiKey);
+
+const model = genAI.getGenerativeModel({
+  model: "gemini-1.5-flash",
+
+});
+
+const generationConfig = {
+  temperature: 1,
+  topP: 0.95,
+  topK: 64,
+  maxOutputTokens: 8192,
+  responseMimeType: "text/plain",
+};
+
+
+async function runChat(prompt) {
+  try {
+    const chatSession = model.startChat({
+      generationConfig,
+      history: [],
+    });
+
+    const result = await chatSession.sendMessage(prompt);
+    const response = result.response;
+
+    if (response && typeof response.text === 'function') {
+      console.log(response.text());
+      return response.text();
+    } else {
+      throw new Error("Invalid response structure");
+    }
+  } catch (error) {
+    console.error("Error in runChat:", error);
+    throw error;
+  }
+}
+
+export default runChat;
